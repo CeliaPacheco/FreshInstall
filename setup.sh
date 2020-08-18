a576fc025a3370d44243959c9dc052889b10c003
Celia Pacheco <pachecoceliam1@gmail.com>
2020-08-18 17:46:12 -0600

    init.vim and guake

--- setup.sh
+++ setup.sh
@@ -10,14 +10,20 @@ flatpak install -y --noninteractive flathub com.spotify.Client;
 flatpak install -y --noninteractive flathub com.mattermost.Desktop; 
 flatpak install -y --noninteractive flathub io.neovim.nvim;
 
-#flatpak run com.spotify.Client; 
-#flatpak run com.mattermost.Desktop; 
-#flatpak run io.neovim.nvim;
-
-#apt install guake;
 
 
 tput setaf 1; echo "Setting up Cinnamon, Log out after"; tput sgr0
 curl https://raw.githubusercontent.com/CeliaPacheco/FreshInstall/master/cinnamon_desktop > cinnamon_desktop
 dconf load /org/cinnamon/ < cinnamon_desktop
 rm cinnamon_desktop
+
+
+tput setaf 1; echo "Downloading Nvim init file"; tput sgr0
+curl https://raw.githubusercontent.com/CeliaPacheco/FreshInstall/master/init.vim > init.vim
+mv init.vim ~/.config/nvim
+
+
+
+tput setaf 1; echo "Installing apt packages"; tput sgr0
+apt install guake;
+
