0aa240252d83d749bcf3cc0cbfb0daf3c9cc5ee9
Celia Pacheco <pachecoceliam1@gmail.com>
2020-08-18 16:17:41 -0600

    Changed to flatpack

--- setup.sh
+++ setup.sh
@@ -2,18 +2,12 @@
 
 tput setaf 1; echo "Update and Upgrade"; tput sgr0
 
-sudo rm /etc/apt/prefrences.d/nosnap.pref
-
 sudo apt update && sudo apt upgrade -y
 
-tput setaf 1; echo "Install snap and snap packages"; tput sgr0
+tput setaf 1; echo "Install flatpack packages"; tput sgr0
 
-sudo apt install snapd
-
-sudo snap install mattermost-desktop spotify htop
-
-sudo snap install --beta nvim --classic
-
+flatpack install flathub com.spotify.Client com.mattermost.Desktop io.neovim.nvim
+flatpack run com.spotify.Client com.mattermost.Desktop io.neovim.nvim
 
 apt install guake
 
