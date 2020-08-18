435c9568ae11a8da253b56e3965626609442b899
Celia Pacheco <pachecoceliam1@gmail.com>
2020-08-18 17:07:11 -0600

    non interactive

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ sudo apt update && sudo apt upgrade -y
 
 tput setaf 1; echo "Install flatpack packages"; tput sgr0
 
-flatpak install -y flathub com.spotify.Client;
-flatpak install -y flathub com.mattermost.Desktop; 
-flatpak install -y flathub io.neovim.nvim;
+flatpak install -y --noninteractive flathub com.spotify.Client;
+flatpak install -y --noninteractive flathub com.mattermost.Desktop; 
+flatpak install -y --noninteractive flathub io.neovim.nvim;
 
 flatpak run com.spotify.Client; 
 flatpak run com.mattermost.Desktop; 
