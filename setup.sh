e04952af9eb115d92d3db61ef8684bd2afe0234d
Celia Pacheco <pachecoceliam1@gmail.com>
2020-08-18 16:57:01 -0600

    Assume yes

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ sudo apt update && sudo apt upgrade -y
 
 tput setaf 1; echo "Install flatpack packages"; tput sgr0
 
-flatpak install flathub com.spotify.Client 
-flatpak install flathub com.mattermost.Desktop 
-flatpak install flathub io.neovim.nvim
+flatpak install -y flathub com.spotify.Client 
+flatpak install -y flathub com.mattermost.Desktop 
+flatpak install -y flathub io.neovim.nvim
 
 flatpak run com.spotify.Client 
 flatpak run com.mattermost.Desktop 
