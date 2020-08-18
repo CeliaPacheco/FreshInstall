8ed82f8003a9fc5e4cc336e082ceb0884d6a286a
Celia Pacheco <pachecoceliam1@gmail.com>
2020-08-18 17:02:38 -0600

    Sytax?

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@ sudo apt update && sudo apt upgrade -y
 
 tput setaf 1; echo "Install flatpack packages"; tput sgr0
 
-flatpak install -y flathub com.spotify.Client 
-flatpak install -y flathub com.mattermost.Desktop 
-flatpak install -y flathub io.neovim.nvim
+flatpak install -y flathub com.spotify.Client;
+flatpak install -y flathub com.mattermost.Desktop; 
+flatpak install -y flathub io.neovim.nvim;
 
-flatpak run com.spotify.Client 
-flatpak run com.mattermost.Desktop 
-flatpak run io.neovim.nvim
+flatpak run com.spotify.Client; 
+flatpak run com.mattermost.Desktop; 
+flatpak run io.neovim.nvim;
 
-apt install guake
+apt install guake;
 
 
 tput setaf 1; echo "Setting up Cinnamon, Log out after"; tput sgr0
