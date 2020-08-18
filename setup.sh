41f6217a08565cda229d53f319d29eb497c8d750
Celia Pacheco <pachecoceliam1@gmail.com>
2020-08-18 15:42:55 -0600

    Added snap packages

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env sh
 
 tput setaf 1; echo "Update and Upgrade"; tput sgr0
+
+sudo rm /etc/apt/prefrences.d/nosnap.pref
+
 sudo apt update && sudo apt upgrade -y
+
+tput setaf 1; echo "Install snap and snap packages"; tput sgr0
+
+sudo apt install snapd
+
+sudo snap install mattermost-desktop spotify htop
+
+sudo snap install --beta nvim --classic
+
+
+apt install guake
+
